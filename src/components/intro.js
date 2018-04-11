6343dc716809c24c434225c6a7bf13ab58b8ec50
jarrodyellets <jarrod524@gmail.com>
2018-04-11 15:55:10 +0200

    Added intro search

--- src/components/intro.js
+++ src/components/intro.js
@@ -7,21 +7,48 @@ class Intro extends Component {
 			day: true
 		}
 
-		this.handleClick = this.handleClick.bind(this);
+		this.handleBuyClick = this.handleBuyClick.bind(this);
+		this.handleRentClick = this.handleRentClick.bind(this);
 	}
 
-	handleClick(){
-		let day = this.state.day;
+	handleBuyClick(){
 		this.setState({
-			day: !day
+			day: true
+		})
+	}
+
+	handleRentClick(){
+		this.setState({
+			day: false
 		})
 	}
 
 	render(){
 		return (
 			<div className={this.state.day ? 'main austinDay' : 'main austinNight'}>
-				<div className="introTitle">Find you new {this.state.day ? 'home' : 'rental'}</div>
-				<button onClick={this.handleClick}>Click Me</button>
+				<div className="titleContainer">
+					<div className="introTitle">Find your new {this.state.day ? 'house' : 'rental'}</div>
+					<div className="buttonIntroDiv">
+						<button className="introButton" onClick={this.handleBuyClick}>Buy</button>
+						<button className="introButton" onClick={this.handleRentClick}>Rent</button>
+					</div>
+					<div className="selectDiv">
+						<select className="introSelect">
+							<option value="" disabled selected>Choose Neighborhood</option>
+							<option value="downtown">Downtown</option>
+							<option value="travis-heights">Travis Heights</option>
+							<option value="east-austin">East Austin</option>
+							<option value="rollingwood">Rollingwood</option>
+							<option value="barton-hills">Barton Hills</option>
+							<option value="westlake">Westlake</option>
+							<option value="tarrytown">Tarrytown</option>
+							<option value="hyde-park">Hyde Park</option>
+							<option value="east-riverside">East Riverside</option>
+							<option value="allandale">Allandale</option>
+						</select>
+						<button className="selectButton">Submit</button>
+					</div>
+				</div>
 			</div>
 			)
 	}

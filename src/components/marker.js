56964fd445887039d3c0662706b5b490b49da3d2
jarrodyellets <jarrod524@gmail.com>
2018-04-19 14:24:02 +0200

    Added popup pics to markers

--- src/components/marker.js
+++ src/components/marker.js
@@ -2,15 +2,12 @@ import React from 'react';
 import Card from './card';
 
 const Marker = (props) => {
-	const popUp = props.neighborhood.filter(function(house){
-		return house.lng == props.lng;
-	});
 	return (
 		<div className={props.stateId == props.id ? "houseIconDiv selectedHouseDiv" : "houseIconDiv"} 
 			 	 onMouseOver={props.changeId.bind(props.changeId, props.id)} 
 			 	 onMouseLeave={props.changeId.bind(props.changeId, null)}>
-			<div className="housePopUp">
-				
+			<div className={props.stateId == props.id ? "housePopUp" : "hidden"}>
+				<img className="popUpImg" src={props.stateId != null ? props.neighborhood[props.stateId].image : null} />
 			</div>
 			<i className={props.stateId == props.id ? "fas fa-home selectedHouseIcon" : "fas fa-home houseIcon"} id={props.id}></i>
 			<div className="priceMap">{props.price}</div>

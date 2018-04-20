de72b1fc27396cdf9c4cb51c5b58b75526f52846
jarrodyellets <jarrod524@gmail.com>
2018-04-20 08:29:50 +0200

    Reformatted hover cards

--- src/components/card.js
+++ src/components/card.js
@@ -12,16 +12,18 @@ class Card extends Component {
 			backgroundSize: 'cover',
 			backgroundPosition: 'center',
 			width: '100%',
-			height: '200px',
+			height: '100%',
 			cursor: 'pointer'
 		}
 		return(
 			<div className="card" style={cardStyle}>
 				<div className="description">
 					<span className="price">{this.props.price} </span>
-					<span className="beds">{this.props.beds} </span>
-					<span className="baths">{this.props.baths} </span>
-					<span className="size">{this.props.size}</span>
+					<div>
+						<span className="beds">{this.props.beds} </span>
+						<span className="baths">{this.props.baths} </span>
+						<span className="size">{this.props.size}</span>
+					</div>
 				</div>
 				<div className="address"> {this.props.address} Austin, TX</div>
 			</div>

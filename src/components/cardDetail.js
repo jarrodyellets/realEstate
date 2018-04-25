c55f68fb76ebf40c27e39d1f18475e1eaf57485b
jarrodyellets <jarrod524@gmail.com>
2018-04-25 12:25:50 +0200

    Added blackout

--- src/components/cardDetail.js
+++ src/components/cardDetail.js
@@ -2,20 +2,22 @@ import React from 'react';
 
 const CardDetail = (props) => {
 	return(
-		<div className="detailContainer">
-			<div className="detailNav">
-				<img className="detailLogo" src="https://www.jarrodyellets.com/images/pillow/pillowLogo.jpg" />
-			  <i onClick={props.changeDetail} className="fas fa-times closeIcon"></i>
-			</div>
-			<img className="detailImage" src={props.house.image} />
-			<div className="detailHouse">
-				<div className="detailPrice">{props.house.price}{props.mode == "rent" ? "/mo" : "" }</div>
-				<div className="detailDetails">
-					<span className="detailBeds">{props.house.bedrooms} </span>
-					<span className="detailBaths">{props.house.bathrooms} </span>
-					<span className="detailSize">{props.house.size}</span>
-				</div> 
-				<div className="detailAddress">{props.house.address} Austin, TX</div>
+		<div className="blackout">
+			<div className="detailContainer">
+				<div className="detailNav">
+					<img className="detailLogo" src="https://www.jarrodyellets.com/images/pillow/pillowLogo.jpg" />
+				  <i onClick={props.changeDetail} className="fas fa-times closeIcon"></i>
+				</div>
+				<img className="detailImage" src={props.house.image} />
+				<div className="detailHouse">
+					<div className="detailPrice">{props.house.price}{props.mode == "rent" ? "/mo" : "" }</div>
+					<div className="detailDetails">
+						<span className="detailBeds">{props.house.bedrooms} </span>
+						<span className="detailBaths">{props.house.bathrooms} </span>
+						<span className="detailSize">{props.house.size}</span>
+					</div> 
+					<div className="detailAddress">{props.house.address} Austin, TX</div>
+				</div>
 			</div>
 		</div>
 		)

92418deea19d8bff45e0a514d23b570df4fd95e8
jarrodyellets <jarrod524@gmail.com>
2018-07-10 00:38:51 +0200

    Change tabs to spaces

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = (props) => {
-    return(
+    return (
         <div className="footerWrapper" aria-label="footer">
             <div className="footerAddress">
                 <div className="addressDiv">
@@ -16,13 +16,13 @@ const Footer = (props) => {
             <div className="author">Jarrod Yellets | 2018</div>
             <div className="footerRight">
                 <div className="footerSocial">
-                        <div>Follow Pillow</div>
-                        <div>
-                            <i className="fab fa-facebook socialFoot"></i>
-                            <i className="fab fa-instagram socialFoot"></i>
-                            <i className="fab fa-twitter-square socialFoot"></i>
-                        </div>
+                    <div>Follow Pillow</div>
+                    <div>
+                        <i className="fab fa-facebook socialFoot"></i>
+                        <i className="fab fa-instagram socialFoot"></i>
+                        <i className="fab fa-twitter-square socialFoot"></i>
                     </div>
+                </div>
             </div>
         </div>
     )

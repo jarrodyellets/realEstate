5fe28be0c7d302896b054741592f44cb88fbcbbf
jarrodyellets <jarrod524@gmail.com>
2018-07-10 00:27:15 +0200

    Refactor footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,28 +3,28 @@ import React from 'react';
 const Footer = (props) => {
     return(
         <div className="footerWrapper" aria-label="footer">
-        <div className="footerAddress">
-            <div className="addressDiv">
-                <div>235 Park Ave</div>
-                <div>Austin, TX</div>
-            </div>
-            <div className="footerContact">
-                <div>jarrod@jarrodyellets.com</div>
-                <div>512-555-2390</div>
-            </div>
-        </div>
-        <div className="author">Jarrod Yellets | 2018</div>
-        <div className="footerRight">
-            <div className="footerSocial">
-                    <div>Follow Pillow</div>
-                    <div>
-                        <i className="fab fa-facebook socialFoot"></i>
-                        <i className="fab fa-instagram socialFoot"></i>
-                        <i className="fab fa-twitter-square socialFoot"></i>
-                    </div>
+            <div className="footerAddress">
+                <div className="addressDiv">
+                    <div>235 Park Ave</div>
+                    <div>Austin, TX</div>
                 </div>
+                <div className="footerContact">
+                    <div>jarrod@jarrodyellets.com</div>
+                    <div>512-555-2390</div>
+                </div>
+            </div>
+            <div className="author">Jarrod Yellets | 2018</div>
+            <div className="footerRight">
+                <div className="footerSocial">
+                        <div>Follow Pillow</div>
+                        <div>
+                            <i className="fab fa-facebook socialFoot"></i>
+                            <i className="fab fa-instagram socialFoot"></i>
+                            <i className="fab fa-twitter-square socialFoot"></i>
+                        </div>
+                    </div>
+            </div>
         </div>
-    </div>
     )
 }
 

68a8f02dbc8608935859bf2284bf378bc4f9b483
jarrodyellets <jarrod524@gmail.com>
2018-10-15 09:47:53 +0200

    Fixed footer margin

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,26 +3,30 @@ import React from 'react';
 const Footer = (props) => {
     return (
         <div className="footerWrapper" aria-label="footer">
-            <div className="footerAddress">
-                <div className="addressDiv">
-                    <div>235 Park Ave</div>
-                    <div>Austin, TX</div>
-                </div>
-                <div className="footerContact">
-                    <div>jarrod@jarrodyellets.com</div>
-                    <div>512-555-2390</div>
-                </div>
-            </div>
-            <div className="author">Jarrod Yellets | 2018</div>
-            <div className="footerRight">
-                <div className="footerSocial">
-                    <div className="footerSocialTitle">Follow Pillow</div>
-                    <div>
-                        <i className="fab fa-facebook socialFoot"></i>
-                        <i className="fab fa-instagram socialFoot"></i>
-                        <i className="fab fa-twitter-square socialFoot"></i>
+            <div className="container">
+              <div className="footerInnerWrapper">
+                <div className="footerAddress">
+                    <div className="addressDiv">
+                        <div>235 Park Ave</div>
+                        <div>Austin, TX</div>
+                    </div>
+                    <div className="footerContact">
+                        <div>jarrod@jarrodyellets.com</div>
+                        <div>512-555-2390</div>
                     </div>
                 </div>
+                <div className="author">Jarrod Yellets | 2018</div>
+                <div className="footerRight">
+                    <div className="footerSocial">
+                        <div className="footerSocialTitle">Follow Pillow</div>
+                        <div>
+                            <i className="fab fa-facebook socialFoot"></i>
+                            <i className="fab fa-instagram socialFoot"></i>
+                            <i className="fab fa-twitter-square socialFoot"></i>
+                        </div>
+                    </div>
+                </div>
+              </div>
             </div>
         </div>
     )
